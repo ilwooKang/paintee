a2ff277659e326b6b5ec94fe74b19ad31f711eff
sweethome <sweethome@sweethomeui-macbook-air.local>
2016-11-18 22:55:06 +0900

    ios

---
+++ www/cordova_plugins.js
@@ -0,0 +1,39 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-dialogs": "1.2.1",
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-plugin-settings-hook": "0.2.4"
+};
+// BOTTOM OF METADATA
+});
